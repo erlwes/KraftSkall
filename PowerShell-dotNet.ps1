6c513a73191503eb6c559d7a422ed947e8fe6917
Erlend <88742199+erlwes@users.noreply.github.com>
2024-10-26 14:08:24 +0200

    Update PowerShell-dotNet.ps1

--- PowerShell-dotNet.ps1
+++ PowerShell-dotNet.ps1
@@ -1,6 +1,6 @@
 # CUSTOM CLASSES
 # Load a non-predefined .Net library - Identify Defined types (Namespace and Classes)
-$dll = "C:\Users\Temp\OneDrive - Westervik\Dokumenter\PowerShell\Modules\PSStringToQRCode\1.0.1\Net.Codecrete.QrCodeGenerator\Net.Codecrete.QrCodeGenerator.dll"
+$dll = "C:\Users\Temp\OneDrive\Dokumenter\PowerShell\Modules\PSStringToQRCode\1.0.1\Net.Codecrete.QrCodeGenerator\Net.Codecrete.QrCodeGenerator.dll"
 $obj = [System.Reflection.Assembly]::LoadFile("$dll")
 $obj.DefinedTypes | select FullName
 
