81f2afb99ef60d3b7aaef8c6b87c4ee9ab470ddc
Erlend <88742199+erlwes@users.noreply.github.com>
2025-01-14 12:31:51 +0100

    Create Random.ps1

---
+++ Random.ps1
@@ -0,0 +1,25 @@
+Get-NetNeighbor -AddressFamily IPv4
+arp -a
+
+Get-NetNeighbor -AddressFamily IPv4 | select -ExpandProperty IPAddress -Unique
+(arp -a | select -Skip 3) -replace '..-..-..-..-..-..' -replace "(static|dynamic)" -replace '\s'
+   
+Get-DnsClientCache
+ipconfig /displaydns
+
+#Get loaded assembly
+[System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object -FilterScript { $_.Location } | Sort-Object -Property FullName
+
+#Install ISE
+Get-WindowsCapability -Name Microsoft.Windows.PowerShell.ISE~~~~0.0.1.0 -Online | Add-WindowsCapability -Online -Verbose
+
+#New ScriptInfo
+New-ScriptFileInfo -Path 'C:\Users\Temp\MyScript.ps1'
+Publish-Script -Path 'C:\Users\Temp\MyScript.ps1' -NuGetApiKey xxxxxxxxxx
+
+
+#CISCO cable test length
+#COM PORT RATE = 115200
+show cable-diagnostics
+test cable-diagnostics tdr interface Fivegigabit1/0/[portnumber]
+
